73d45995ab9efc8e9ad1aa99234bd5b86e92149f
Russell J <proxydox3@gmail.com>
2025-05-04 16:43:00 -0400

    Update Editor.jsx
    Fixed terminal showing while PDF tab was active

--- frontend/src/Editor.jsx
+++ frontend/src/Editor.jsx
@@ -123,29 +123,31 @@ const Editor = ({ currentTab, setCurrentTab }) => {
             <div className="main-editor-content" style={activeTab.language === 'pdf' ? { padding: 0 } : {}}>
                 {renderContent()}
             </div>
-            <div className="terminal-wrapper">
-                <div className="terminal-header">TERMINAL</div>
-                <div className="terminal-content">
-                    <div className="terminal-lines">
-                    PS C:\Users\rjoar\Documents\GitHub\portfolio&gt; <span> </span>
-                        <Typewriter
-                        speed = {150}
-                        text = "echo $intro_msg"
-                        onComplete={() => {
-                            document.getElementById('read-below-message').style.display = 'block';
-                        }}
-                        />
-                        <br></br>
-                        <div id="read-below-message" style={{ display: 'none' }}>
-                            ====== Read Below ======<br></br>
-                            Hey! If you have a hard time reading code,<br></br>
-                            click the RUN button in the top right<br></br>
-                            of the header to see the content better!<br></br>
-                            <Cursor/>
+            {activeTab.language !== 'pdf' && (
+                <div className="terminal-wrapper">
+                    <div className="terminal-header">TERMINAL</div>
+                    <div className="terminal-content">
+                        <div className="terminal-lines">
+                            PS C:\Users\rjoar\Documents\GitHub\portfolio&gt; <span> </span>
+                            <Typewriter
+                                speed={150}
+                                text="echo $intro_msg"
+                                onComplete={() => {
+                                    document.getElementById('read-below-message').style.display = 'block';
+                                }}
+                            />
+                            <br></br>
+                            <div id="read-below-message" style={{ display: 'none' }}>
+                                ====== Read Below ======<br></br>
+                                Hey! If you have a hard time reading code,<br></br>
+                                click the RUN button in the top right<br></br>
+                                of the header to see the content better!<br></br>
+                                <Cursor/>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
+            )}
         </div>
     );
 };
